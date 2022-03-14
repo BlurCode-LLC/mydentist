41c82c34041f4f50b1d15d80a5000ac0f089c53a
abrornomos <abrornomos2018@gmail.com>
2022-03-14 18:56:38 +0500

    API begin

--- baseapp/urls.py
+++ baseapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from . import api
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/', api.index, name='api_index'),
     path('results/', views.results, name='results'),
+    # path('results/', views.results, name='results'),
     path('get_dentists/', views.get_dentists, name='get_dentists'),
+    # path('get_dentists/', views.get_dentists, name='get_dentists'),
 ]
