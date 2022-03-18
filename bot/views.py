f8c35ce81fc37f08ea7d983faaffd87dad77ffad
abrornomos <abrornomos2018@gmail.com>
2022-03-18 14:29:39 +0000

    Webhook small addon

--- bot/views.py
+++ bot/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from telebot import TeleBot, types
-# import requests
+import requests
 
 
 bot = TeleBot(settings.TELEGRAM_BOT_TOKEN)
@@ -11,15 +11,13 @@ bot = TeleBot(settings.TELEGRAM_BOT_TOKEN)
 
 @csrf_exempt
 def tgbot(request):
-    result = request.body.decode("UTF-8")
-    # result = requests.get("https://api.telegram.org/bot%(token)s/getUpdates" % {
-    #     'token': bot.token
-    # }).json()
-    # print(result)
-    update = types.Update.de_json(result)
-    bot.process_new_updates([update])
+    if request.method == "POST":
+        result = request.body.decode("utf-8")
+        update = types.Update.de_json(loads(result))
+        bot.process_new_updates([update])
 
-    return JsonResponse(loads(result), status=200)
+        return HttpResponse("")
+    return JsonResponse(requests.get("https://api.telegram.org/bot%s/getMe" % bot.token).json(), status=200)
 
 
 @bot.message_handler(commands=["start"])
