ec3adbc6e5c6790950cae5b0f90b95a6adc015d0
abrornomos <abrornomos2018@gmail.com>
2022-03-19 23:08:34 +0500

    Bot

--- bot/views.py
+++ bot/views.py
@@ -1,6 +1,6 @@
 from json import loads
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from telebot import TeleBot, types
 import requests
@@ -12,23 +12,11 @@ bot = TeleBot(settings.TELEGRAM_BOT_TOKEN)
 @csrf_exempt
 def tgbot(request):
     if request.method == "POST":
-<<<<<<< HEAD
         result = request.body.decode("utf-8")
         update = types.Update.de_json(loads(result))
         bot.process_new_updates([update])
 
         return HttpResponse("")
-=======
-        result = request.body.decode("UTF-8")
-        # result = requests.get("https://api.telegram.org/bot%(token)s/getUpdates" % {
-        #     'token': bot.token
-        # }).json()
-        # print(result)
-        update = types.Update.de_json(result)
-        bot.process_new_updates([update])
-
-        return JsonResponse(loads(result), status=200)
->>>>>>> origin/dev
     return JsonResponse(requests.get("https://api.telegram.org/bot%s/getMe" % bot.token).json(), status=200)
 
 
